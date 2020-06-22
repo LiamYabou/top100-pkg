c28ffb7339f0c6297acdc0ace930652e4c287556
liam <liamyabou@outlook.com>
2020-06-22 15:34:47 +0800

    Revise the `Open` method.

--- rabbitmq/conn.go
+++ rabbitmq/conn.go
@@ -2,15 +2,10 @@ package rabbitmq
 
 import (
 	"os"
-
 	"github.com/streadway/amqp"
 )
 
-var (
-	amqpURL = os.Getenv("AMQP_URL")
-)
-
-func Open() (amqpConn *amqp.Connection, err error) {
+func Open(amqpURL string) (amqpConn *amqp.Connection, err error) {
 	amqpConn, err = amqp.Dial(amqpURL)
 	return amqpConn, err
 }
