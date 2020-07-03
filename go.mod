e25002050428ed305f1b4f62e692b62f8b96f1c1
liam <liamyabou@outlook.com>
2020-07-03 16:56:59 +0800

    Release v0.0.2-configure the New Relic.

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/getsentry/sentry-go v0.6.1
 	github.com/jackc/pgx/v4 v4.6.0
 	github.com/lib/pq v1.6.0
+	github.com/newrelic/go-agent/v3 v3.8.0
 	github.com/sirupsen/logrus v1.6.0
 	github.com/streadway/amqp v0.0.0-20200108173154-1c71cc93ed71
 	github.com/stretchr/testify v1.6.0
--- monitor/base.go
+++ monitor/base.go
@@ -1,8 +1,20 @@
 package monitor
 
 import (
+  "github.com/newrelic/go-agent/v3/newrelic"
   "github.com/getsentry/sentry-go"
 )
+
+func InitNewRelic(env string, appName string, licenseKey string) (newRelicApp *newrelic.Application, err error) {
+	if env == "development" {
+		return
+	}
+	newRelicApp, err = newrelic.NewApplication(
+		newrelic.ConfigAppName(appName),
+		newrelic.ConfigLicense(licenseKey),
+	)
+	return
+}
 
 func InitSentry(env string) (err error) {
 	if env == "development" {
